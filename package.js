661f0acc0db99f9872728098c45b396974f61973
Rocky Assad <rassad@gbhem.org>
2015-02-20 13:12:22 -0600

    minor version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'fourq:babel',
   summary: 'Turn ES6 code into readable vanilla ES5 with source maps',
   git: 'https://github.com/fourq/meteor-fourq-babel',
-  version: '2.0.0'
+  version: '2.0.1'
 });
 
 
