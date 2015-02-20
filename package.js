c80d836f35e7109408e7eb9ee29412526557dfd5
Rocky Assad <rassad@gbhem.org>
2015-02-20 13:11:36 -0600

    add github repo to package describe

--- package.js
+++ package.js
@@ -1,6 +1,7 @@
 Package.describe({
   name: 'fourq:babel',
   summary: 'Turn ES6 code into readable vanilla ES5 with source maps',
+  git: 'https://github.com/fourq/meteor-fourq-babel',
   version: '2.0.0'
 });
 
